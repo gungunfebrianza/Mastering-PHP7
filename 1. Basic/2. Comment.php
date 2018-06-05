24274ca04f2008cc577898b1fa9067f4197391cb
Gun Gun Febrianza <pusatrisetteknologi@gmail.com>
2018-06-05 22:32:44 +0700

    Add Comment Example

---
+++ 1. Basic/2. Comment.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/*
+This is a multiple-lines comment block
+that spans over multiple
+lines
+*/
+
+// You can also use comments to leave out parts of a code line
+$x = 5 /* + 15 */ + 5;
+echo $x;
+?>
+
+</body>
+</html> 
