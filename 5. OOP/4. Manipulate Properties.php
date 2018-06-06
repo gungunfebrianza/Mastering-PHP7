6feebc94112a1cc9b77b629e38f821cfaf8421b8
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-06-07 05:52:36 +0700

    Add Example : Properties OOP

---
+++ 5. OOP/4. Manipulate Properties.php
@@ -0,0 +1,12 @@
+<?php
+class ShopProduct
+{
+    public $title = "default product";
+    public $producerMainName = "main name";
+    public $producerFirstName = "first name";
+    public $price = 0;
+}
+  $product1 = new ShopProduct();
+  $product2 = new ShopProduct();
+  $product1->title="Bitcoin";
+  $product2->title="Cocoa";
