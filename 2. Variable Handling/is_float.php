32a8d77ca98908cf8647c1e5a602f7ce3b2d31d9
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-06-06 17:15:44 +0700

    Add Example : float

---
+++ 2. Variable Handling/is_float.php
@@ -0,0 +1,8 @@
+<?php
+var_dump(is_float(27.25));
+var_dump(is_float('abc'));
+var_dump(is_float(23));
+var_dump(is_float(23.5));
+var_dump(is_float(1e7));  //Scientific Notation
+var_dump(is_float(true));
+?>
