ffe000fae5d4eb4f1987d2277ed328cb692fb743
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-06-06 13:40:34 +0700

    Add Example : View Variable

---
+++ 2. Variable Handling/View Variable.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt = "W3Schools.com";
+echo "I love $txt!";
+
+$txt = "W3Schools.com";
+echo "I love " . $txt . "!";
+
+$x = 5;
+$y = 4;
+echo $x + $y;
+
+?>
+
+</body>
+</html>
