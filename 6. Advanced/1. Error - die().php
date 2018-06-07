df0e118e7ba1459a6ad75684dae681ef471a17af
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-06-07 13:13:17 +0700

    Update Example : Error

--- 6. Advanced/1. Error - die().php
+++ 6. Advanced/1. Error - die().php
@@ -1,6 +1,7 @@
 <?php
 if (!file_exists("welcome.txt")) {
     die("File not found");
+    echo "You can not see me because script has been stopped!";
 } else {
     $file=fopen("welcome.txt", "r");
 }
