8f88770640927a312dfb3aed3e4495519448abc2
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-06-08 02:18:13 +0700

    Add Switch

---
+++ 7. Conditional/Switch - Integer.php
@@ -0,0 +1,15 @@
+
+<?php
+
+switch ($i) {
+    case 0:
+        echo "i equals 0";
+        break;
+    case 1:
+        echo "i equals 1";
+        break;
+    case 2:
+        echo "i equals 2";
+        break;
+}
+?>
