4c5947ce37ca9f5dad7a74db14a63224fc863039
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-06-08 00:43:01 +0700

    Add Example : Cookies

---
+++ 7. Cookies/Example - Cookies.php
@@ -0,0 +1,19 @@
+<?php
+$cookie_name = "user";
+$cookie_value = "John Doe";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+?>
+<html>
+<body>
+
+<?php
+if (!isset($_COOKIE[$cookie_name])) {
+    echo "Cookie named '" . $cookie_name . "' is not set!";
+} else {
+    echo "Cookie '" . $cookie_name . "' is set!<br>";
+    echo "Value is: " . $_COOKIE[$cookie_name];
+}
+?>
+
+</body>
+</html> 
