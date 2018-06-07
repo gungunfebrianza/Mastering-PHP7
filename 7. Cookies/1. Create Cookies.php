71c17f09970a3a08fcc8aac15b2bf18867fdc98c
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-06-08 00:40:27 +0700

    Add New Parameter

--- 7. Cookies/1. Create Cookies.php
+++ 7. Cookies/1. Create Cookies.php
@@ -1,6 +1,6 @@
 <?php
 //the cookie expires after one hour.
-setcookie("lastvisit", date("H:i:s"), time() + 3600);
+setcookie("lastvisit", date("H:i:s"), time() + 3600, "/7. Cookies");
 
 if (isset($_COOKIE['lastvisit'])) {
     echo "Last visit: " . $_COOKIE['lastvisit'];
