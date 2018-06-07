8d294b1329736a96fc6b000a42db64f8eba975d7
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-06-07 23:10:12 +0700

    Add Example : Hash Function

---
+++ 13. Hash Function/Hash - GOST() Algorithm.php
@@ -0,0 +1,10 @@
+<?php
+  $token = hash('gost', '0609458237');
+  echo $token;
+  if ($token == '162d7b1f4a47d0f9de80650a72f6b764e07d52bdd53597a2cb4beba7f8d8954f91dc43f54fb9f9a9') {
+    echo "true";
+  }
+  else {
+    echo "booo";
+  }
+?>
