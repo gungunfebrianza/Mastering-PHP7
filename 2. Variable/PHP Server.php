3cad7fc3ac6d5da2cd64b5e3a67f4dbaf3073dbc
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-06-08 00:57:40 +0700

    Add Example :

---
+++ 2. Variable/PHP Server.php
@@ -0,0 +1,26 @@
+<?php
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_REFERER'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+echo "<br>";
+echo $_SERVER['REMOTE_ADDR'];
+echo "<br>";
+echo $_SERVER['REMOTE_PORT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_FILENAME'];
+echo "<br>";
+echo $_SERVER['REQUEST_METHOD'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['SERVER_SOFTWARE'];
+echo "<br>";
+echo $_SERVER['SERVER_ADDR'];
