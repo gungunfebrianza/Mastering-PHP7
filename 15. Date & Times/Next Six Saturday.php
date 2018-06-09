3cb61dd611fb6fffc585adb5afdc06a56133a20a
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-06-09 18:40:51 +0700

    Add Example : Time Programming

---
+++ 15. Date & Times/Next Six Saturday.php
@@ -0,0 +1,8 @@
+<?php
+$startdate = strtotime("Saturday");
+$enddate = strtotime("+6 weeks", $startdate);
+
+while ($startdate < $enddate) {
+    echo date("M d", $startdate) . "<br>";
+    $startdate = strtotime("+1 week", $startdate);
+}
