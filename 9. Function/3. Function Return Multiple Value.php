38588ec90db27ddbf259ab7b35833f6167349143
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-06-09 17:34:20 +0700

    Add Example : Function Return Array

---
+++ 9. Function/3. Function Return Multiple Value.php
@@ -0,0 +1,12 @@
+<?php
+$names = fix_names("Gun GUN", "FebrianZa", "SheillA");
+echo $names[0] . " " . $names[1] . " " . $names[2];
+
+function fix_names($n1, $n2, $n3)
+{
+    $n1 = ucfirst(strtolower($n1));
+    $n2 = ucfirst(strtolower($n2));
+    $n3 = ucfirst(strtolower($n3));
+
+    return array($n1, $n2, $n3);
+}
