e9947dee319a3924cb70ab1dc04c583e7806265f
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-06-09 18:39:55 +0700

    Update Example : strtotime()

--- 15. Date & Times/strtotime.php
+++ 15. Date & Times/strtotime.php
@@ -1,3 +1,12 @@
 <?php
 $d=strtotime("10:30pm April 15 2014");
 echo "Created date is " . date("Y-m-d h:i:sa", $d);
+
+$d=strtotime("tomorrow");
+echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+$d=strtotime("next Saturday");
+echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+$d=strtotime("+3 Months");
+echo date("Y-m-d h:i:sa", $d) . "<br>";
