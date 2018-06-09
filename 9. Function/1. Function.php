c15ca2bc0040025580f45f8d46bc4e4f8631b9c2
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-06-09 17:30:36 +0700

    Add Example : Function Parameter

--- 9. Function/1. Function.php
+++ 9. Function/1. Function.php
@@ -1,11 +1,6 @@
 <?php
-echo fix_names("Gun GUN", "FebrianZa", "SheillA");
-
-function fix_names($n1, $n2, $n3)
+function hello()
 {
-    $n1 = ucfirst(strtolower($n1));
-    $n2 = ucfirst(strtolower($n2));
-    $n3 = ucfirst(strtolower($n3));
-
-    return $n1 . " " . $n2 . " " . $n3;
+    echo "Hello World!";
 }
+hello();
