13f4515a5157f51e3b05c9f8692a33c278975f9a
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-06-10 16:57:07 +0700

    Add Example : in_array Array Needle

---
+++ 6. Array/in_array() Array Needle.php
@@ -0,0 +1,14 @@
+<?php
+$a = array(array('p', 'h'), array('p', 'r'), 'o');
+
+if (in_array(array('p', 'h'), $a)) {
+    echo "'ph' was found\n";
+}
+
+if (in_array(array('f', 'i'), $a)) {
+    echo "'fi' was found\n";
+}
+
+if (in_array('o', $a)) {
+    echo "'o' was found\n";
+}
