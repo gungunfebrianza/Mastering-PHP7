50e5ba9a6ee8171ab609b8f6df51a537456f814e
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-06-10 16:52:17 +0700

    Add Example : Array & Cookie

---
+++ 15. Cookies/4. Check Cookie.php
@@ -0,0 +1,6 @@
+<?php
+if (count($_COOKIE) > 0) {
+    echo "Cookies are enabled.";
+} else {
+    echo "Cookies are disabled.";
+}
---
+++ 6. Array/in_array().php
@@ -0,0 +1,10 @@
+
+<?php
+$os = array("Mac", "NT", "Irix", "Linux");
+if (in_array("Irix", $os)) {
+    echo "Got Irix";
+}
+if (in_array("mac", $os)) {
+    echo "Got mac";
+}
+?>
